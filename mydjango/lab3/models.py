ebd5cfd91e88c89ba1baf20d074928a5d146e752
vorobmaks <146077660+vorobmaks@users.noreply.github.com>
2025-05-28 14:57:45 +0300

    Lab5

--- mydjango/lab3/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Team(models.Model):
-    team_name = models.CharField(max_length=45, blank=False, unique=True)
-    stadium = models.CharField(max_length=45, blank=False)
-    city = models.CharField(max_length=45)
-
-    def __str__(self):
-        return self.team_name
